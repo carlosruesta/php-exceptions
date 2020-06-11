ed0a8ed7997e49088c69911fa4348f2faaffca7b
Carlos Ruesta <carlos@dcide.com.br>
2020-06-11 19:23:34 -0300

    Nesta aula, aprendemos:
    Que problemas podem acontecer durante o desenvolvimento
    Como o PHP reporta o acontecimento destes problemas através de exceções
    A tratar possíveis erros, utilizando try - catch
    A tratar mais de uma exceção por vez, utilizando o multi-catch, com o operador |

--- pilha.php
+++ pilha.php
@@ -3,13 +3,30 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+
+    try {
+//            $arrayFixo = new SplFixedArray(2);
+//            $arrayFixo[3] = 1111;
+
+
+
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $error) {
+        echo "Estou na função 1, mas aconteceu um erro na função 2 na linha {$error->getLine()} - Mensagem: {$error->getMessage()}" . PHP_EOL;
+    }
+
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $divisao = intdiv(5,0);
+
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 1111;
+
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
